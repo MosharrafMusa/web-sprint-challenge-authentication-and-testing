aa3f7da822acd5111fccdf66f4dbf4aeee64386d
Mosharraf Musa <mosharrafny@gmail.com>
2020-07-17 20:05:57 -0400

    Register and Login Working

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -4,5 +4,21 @@
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  // tokens are normally sent as the Authorisation header
+  const token = req.headers.authorization;
+  const secret = secrets.jwtSecret;
+  if (token) {
+    // verify that the token is valid
+    jwt.verify(token, secret, (error, decodedToken) => {
+      // if everything is good with the token, the error will be undefined
+      if (error) {
+        res.status(401).json({ you: "shall not pass!" });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "Please provide credentials" });
+  }
 };
